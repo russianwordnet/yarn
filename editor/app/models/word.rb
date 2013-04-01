58f6d8aae493a73774b79d375db4f03c891602be
Dmitry Ustalov <dmitry@eveel.ru>
2013-04-01 14:43:49 +0600

    Do not make magic with Word

--- editor/app/models/word.rb
+++ editor/app/models/word.rb
@@ -1,14 +1,6 @@
 class Word < ActiveRecord::Base
-  # attr_accessible :title, :body
   attr_accessible :word, :grammar, :accents, :uris
 
   serialize :accents, JSON
   serialize :uris, JSON
-
-  before_save :update_revision
-
-  def update_revision
-    self.revision = (self.revision.presence || 0) + 1
-    self.approver_id = self.approved_at = nil
-  end
 end
