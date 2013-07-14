64e878c7fc13aa58a2020662825e2e56e4237329
Dmitry Ustalov <dmitry@eveel.ru>
2013-07-15 04:32:53 +0600

    YarnHistory draft

--- editor/config/application.rb
+++ editor/config/application.rb
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require File.expand_path('../../lib/yarn_history', __FILE__)
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
---
+++ editor/lib/yarn_history.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+# A mixin that allows to simplify history tracking of model changes.
+#
+module YarnHistory
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def has_history(association, attributes, options = {})
+      self.has_many association, options
+      attr_reader :yarn_has_history
+      @yarn_has_history = attributes
+    end
+
+    def history_of(association, attributes, options = {})
+      belongs_to association, options
+      attr_reader :yarn_history_of
+      @yarn_history_of = attributes
+    end
+  end
+
+  module InstanceMethods
+    def update_history(new_model)
+      self.class.transaction do
+        origin = self.history_class.save_history(self)
+
+        yarn_has_history.each do |attr|
+          setter = ('%s=' % attr).to_sym
+          self.send(setter, new_model.send(attr))
+        end
+
+        self.revision += 1
+
+        return self.save.tap { |saved| origin.save! if saved }
+      end
+    end
+
+    def save_history(origin)
+      self.history_association.build do |model|
+        yarn_history_of.each do |attr|
+          setter = ('%s=' % attr).to_sym
+          model.send(setter, origin.send(attr))
+        end
+
+        model.revision = origin.revision
+        model.created_at = origin.updated_at
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:extend, YarnHistory)
