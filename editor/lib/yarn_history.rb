19dcd123c4ed051a1713c82d048d5fc28a818691
Dmitry Ustalov <dmitry@eveel.ru>
2013-07-21 03:23:27 +0600

    Eliminate the YarnHistory warning

--- editor/lib/yarn_history.rb
+++ editor/lib/yarn_history.rb
@@ -19,32 +19,30 @@ module YarnHistory
     end
   end
 
-  module InstanceMethods
-    def update_history(new_model)
-      self.class.transaction do
-        origin = self.history_class.save_history(self)
+  def update_history(new_model)
+    self.class.transaction do
+      origin = self.history_class.save_history(self)
 
-        yarn_has_history.each do |attr|
-          setter = ('%s=' % attr).to_sym
-          self.send(setter, new_model.send(attr))
-        end
-
-        self.revision += 1
-
-        return self.save.tap { |saved| origin.save! if saved }
+      yarn_has_history.each do |attr|
+        setter = ('%s=' % attr).to_sym
+        self.send(setter, new_model.send(attr))
       end
+
+      self.revision += 1
+
+      return self.save.tap { |saved| origin.save! if saved }
     end
+  end
 
-    def save_history(origin)
-      self.history_association.build do |model|
-        yarn_history_of.each do |attr|
-          setter = ('%s=' % attr).to_sym
-          model.send(setter, origin.send(attr))
-        end
-
-        model.revision = origin.revision
-        model.created_at = origin.updated_at
+  def save_history(origin)
+    self.history_association.build do |model|
+      yarn_history_of.each do |attr|
+        setter = ('%s=' % attr).to_sym
+        model.send(setter, origin.send(attr))
       end
+
+      model.revision = origin.revision
+      model.created_at = origin.updated_at
     end
   end
 end
