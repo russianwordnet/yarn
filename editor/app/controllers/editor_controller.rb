f284553408e244584dc4e04ae3dd61e285c8cbb3
Dmitry Ustalov <dmitry@eveel.ru>
2013-10-01 22:52:58 +0600

    Fuck you, Ruby core

--- editor/app/controllers/editor_controller.rb
+++ editor/app/controllers/editor_controller.rb
@@ -102,8 +102,8 @@ class EditorController < ApplicationController
 
     @raw_synsets = @word.raw_synset_words.map(&:synsets).flatten.uniq
     @definitions = @raw_synsets.map(&:definitions).flatten.uniq
-    @synset_words = @raw_synsets.map(&:words).flatten.uniq(&:word_id).
-      reject! { |sw| sw.word_id == @word }
+    @synset_words = @raw_synsets.map(&:words).flatten.uniq(&:word_id)
+    @synset_words.reject! { |sw| sw.word_id == @word }
     @synsets = @word.synset_words.map(&:synsets).flatten.uniq
 
     respond_with @word, @definitions, @synsets
