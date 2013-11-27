232b08b9771f6c64435688daea1b1a52971fa0a5
Dmitry Ustalov <dmitry@eveel.ru>
2013-11-27 13:51:30 +0500

    Actually, map's there

--- editor/app/controllers/words_controller.rb
+++ editor/app/controllers/words_controller.rb
@@ -138,7 +138,7 @@ class WordsController < ApplicationController
       return false
     end
 
-    @query = params[:q].split.map! { |s| ('^%s.+' % s).
+    @query = params[:q].split.map! { |s| ('%s.+' % s).
       gsub(/[её]/, '(е|ё)').
       gsub(/[ЕЁ]/, '(Е|Ё)') }.join ' '
   end
