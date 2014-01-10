4d82b7c764264b194e9b31607f5ea0f001903c46
Dmitry Ustalov <dmitry@eveel.ru>
2014-01-10 07:26:54 +0600

    Do not show removed words on synsets

--- editor/app/helpers/synsets_helper.rb
+++ editor/app/helpers/synsets_helper.rb
@@ -1,5 +1,5 @@
 module SynsetsHelper
   def synset_words(synset)
-    synset.words.map(&:word).map(&:word).join ', '
+    synset.words.map(&:word).delete_if(&:deleted_at).map(&:word).join ', '
   end
 end
