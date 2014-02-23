a7f0cab3deab27c8d46e6968d526dc764fcddd75
Ilya Menshikov <unkmas@gmail.com>
2014-02-23 16:20:39 +0600

    Smart way to create old_synset_word

--- editor/app/models/old_synset_word.rb
+++ editor/app/models/old_synset_word.rb
@@ -9,17 +9,12 @@ class OldSynsetWord < ActiveRecord::Base
   belongs_to :origin, class_name: 'SynsetWord', foreign_key: 'synset_word_id',
     :inverse_of => :old_synset_words
 
-  def self.from_synset_word(synset_word)
-    old_synset_word = synset_word.old_synset_words.build
-    old_synset_word.word = synset_word.word
-    old_synset_word.nsg = synset_word.nsg
-    old_synset_word.marks_ids = synset_word.marks_ids
-    old_synset_word.samples_ids = synset_word.samples_ids
-    old_synset_word.author_id = synset_word.author_id
-    old_synset_word.approver_id = synset_word.approver_id
-    old_synset_word.approved_at = synset_word.approved_at
-    old_synset_word.revision = synset_word.revision
-    old_synset_word.created_at = synset_word.updated_at
-    return old_synset_word
-  end
+   def self.from_synset_word(synset_word)
+    attrs = synset_word.attributes.reject {|k,_| %w(id updated_at).include? k }
+    attrs.merge!(created_at: synset_word.updated_at)
+
+    old_synset_word = synset_word.old_synset_words.build(attrs, without_protection: true)
+
+    old_synset_word
+   end
 end
