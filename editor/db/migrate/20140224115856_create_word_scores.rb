5980e4dfc3b0a32714b9d0f26fdde99104509353
Dmitry Ustalov <dmitry@eveel.ru>
2014-02-28 05:48:02 +0600

    Fix the word_scores creation query

--- editor/db/migrate/20140224115856_create_word_scores.rb
+++ editor/db/migrate/20140224115856_create_word_scores.rb
@@ -1,13 +1,13 @@
 class CreateWordScores < ActiveRecord::Migration
   def up
-    # it hangs when the multi-line approach is used :(
-    execute \
-      'CREATE MATERIALIZED VIEW word_scores AS ' \
-        'SELECT current_words.id AS word_id, ' \
-          'yarn_word_score(current_words.id) AS score, ' \
-          'LOCALTIMESTAMP AS created_at ' \
-        'FROM current_words ' \
-      'WITH DATA;'
+    execute <<-SQL
+      CREATE MATERIALIZED VIEW word_scores AS
+        SELECT current_words.id AS word_id,
+          yarn_word_score(current_words.id) AS score,
+          LOCALTIMESTAMP AS created_at
+        FROM current_words
+      WITH NO DATA;
+    SQL
 
     add_index :word_scores, :word_id
   end
