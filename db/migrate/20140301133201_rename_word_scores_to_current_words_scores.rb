08534b2cae8d63369fc4994127cda5325b6ea8b2
Dmitry Ustalov <dmitry@eveel.ru>
2014-03-01 20:57:50 +0600

    Rename word_scores to current_words_scores tenderly

--- db/migrate/20140301133201_rename_word_scores_to_current_words_scores.rb
+++ db/migrate/20140301133201_rename_word_scores_to_current_words_scores.rb
@@ -1,9 +1,21 @@
 class RenameWordScoresToCurrentWordsScores < ActiveRecord::Migration
   def up
+    remove_index :word_scores, :word_id if index_exists? :word_scores, :word_id
+    remove_index :word_scores, :score if index_exists? :word_scores, :score
+
     execute 'ALTER MATERIALIZED VIEW word_scores RENAME TO current_words_scores;'
+
+    add_index :current_words_scores, :word_id, unique: true
+    add_index :current_words_scores, :score
   end
 
   def down
+    remove_index :current_words_scores, :word_id
+    remove_index :current_words_scores, :score
+
     execute 'ALTER MATERIALIZED VIEW current_words_scores RENAME TO word_scores;'
+
+    add_index :word_scores, :word_id, unique: true
+    add_index :word_scores, :score
   end
 end
