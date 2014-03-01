22a9a1d8369329bcc956fd8ad8fed32b86a553d5
Dmitry Ustalov <dmitry@eveel.ru>
2014-03-01 16:39:54 +0600

    Rename bring_the_indexes_back_home to bring_the_examples_indexes_back_home

--- db/migrate/20140301093635_bring_the_examples_indexes_back_home.rb
+++ db/migrate/20140301093635_bring_the_examples_indexes_back_home.rb
@@ -1,9 +1,13 @@
-class BringTheIndexesBackHome < ActiveRecord::Migration
+class BringTheExamplesIndexesBackHome < ActiveRecord::Migration
   def up
     execute 'ALTER INDEX current_samples_pkey RENAME TO current_examples_pkey'
     execute 'ALTER INDEX samples_pkey RENAME TO examples_pkey'
 
     rename_column :examples, :sample_id, :example_id
+    add_index :examples, :example_id
+    add_foreign_key :examples, :current_examples,
+      :column => :example_id,
+      :dependent => :delete
 
     %w(current_examples examples).each do |table_name|
       add_index table_name, :author_id
@@ -27,10 +31,6 @@ class BringTheIndexesBackHome < ActiveRecord::Migration
 
     add_index :current_examples, :updated_at
     add_index :examples, :created_at
-
-    add_foreign_key :examples, :current_examples,
-      :column => :example_id,
-      :dependent => :delete
   end
 
   def down
