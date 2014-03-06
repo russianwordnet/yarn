c610c79bbce0dd38a967be1f04f7d1adb93b5f91
Dmitry Ustalov <dmitry@eveel.ru>
2014-03-07 00:01:46 +0600

    Less deprecation warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yarn::Application.config.secret_token = '2ce4017cb5eff86abfbc7e0532fa47a5fc1f10f22666f4b0e86920d9c8e161234bdc6d1c292970875b61d89d8033f4752f9c16f67215f1ba14ee4a2407a33596'
+Yarn::Application.config.secret_key_base =
+Yarn::Application.config.secret_token =
+'2ce4017cb5eff86abfbc7e0532fa47a5fc1f10f22666f4b0e86920d9c8e161234bdc6d1c292970875b61d89d8033f4752f9c16f67215f1ba14ee4a2407a33596'
