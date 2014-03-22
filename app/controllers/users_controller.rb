20dd9b5e70f2d4b60e153b1a0c8f4ddcb86228d9
Dmitry Ustalov <dmitry@eveel.ru>
2014-03-23 04:38:35 +0600

    Finally, fix this error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @stats = User.scores(current_user.id).first
+    @synsets = Synset.retrieve_creators.
+      find { |u, _| u == current_user }.
+      try(:last)
   end
 end
