a98a9172bb5ed20629791a24ddabd79396c2ba7a
Dmitry Ustalov <dmitry@eveel.ru>
2014-06-13 18:14:31 +0600

    Link public/yarn.xml, too

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch, 'develop'
 set :deploy_to, '/home/dmchk/yarn'
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml public/yarn.xml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
