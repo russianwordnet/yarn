1a7cefd1e3318bcdf4416b07739cfe8eb7958c9e
Dmitry Ustalov <dmitry@eveel.ru>
2014-08-27 23:57:02 +0600

    Specify the revision file for Squash

--- config/initializers/squash.rb
+++ config/initializers/squash.rb
@@ -1,3 +1,4 @@
 Squash::Ruby.configure api_host: Rails.configuration.local['squash_host'],
                        api_key: Rails.configuration.local['squash_key'],
+                       revision_file: Rails.root.join('REVISION'),
                        disabled: !Rails.env.production?
