01974b58d4b4672aebb6706527db38c05c52eff8
sld <sld7700@gmail.com>
2014-09-15 22:17:34 +0400

    Fixed 2 failing migrations.

--- db/migrate/20131216185542_index_current_words_trgm.rb
+++ db/migrate/20131216185542_index_current_words_trgm.rb
@@ -1,6 +1,8 @@
 class IndexCurrentWordsTrgm < ActiveRecord::Migration
   def up
-    execute 'CREATE INDEX index_current_words_on_word_trgm ON current_words USING gin (word gin_trgm_ops);'
+    execute "CREATE extension pg_trgm;"
+    execute "CREATE INDEX index_current_words_on_word_trgm ON current_words \
+      USING gin (word gin_trgm_ops);"
   end
 
   def down
