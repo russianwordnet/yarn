c9ec80acbfbfe8fbc1984fc4048c244de07b238b
Dmitry Ustalov <dmitry@eveel.ru>
2014-10-30 04:03:05 +0500

    Make SubsumptionAnswer usable

--- app/models/subsumption_answer.rb
+++ app/models/subsumption_answer.rb
@@ -1,8 +1,9 @@
 class SubsumptionAnswer < ActiveRecord::Base
-  attr_accessible :assignment, :answer
+  attr_accessible :assignment, :assignment_id, :answer
 
   belongs_to :assignment, class_name: 'SubsumptionAssignment'
   belongs_to :user
 
+  validates_uniqueness_of :assignment, :scope => :user
   validates_inclusion_of :answer, in: %w(yes no report), allow_nil: true
 end
