3089217be52a9c203cf3887323afe66eba2302b4
Dmitry Ustalov <dmitry@eveel.ru>
2014-12-24 23:46:15 +0500

    Show recently edited synsets for a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,5 +24,7 @@ class UsersController < ApplicationController
     @stats = User.scores(@user.id).first
     @synsets = Synset.joins_initiators.where(deleted_at: nil,
       current_synsets_initiators: { initiator_id: @user })
+    @recent_synsets = Synset.where(deleted_at: nil, author_id: @user).
+      order('updated_at DESC').limit(50)
   end
 end
